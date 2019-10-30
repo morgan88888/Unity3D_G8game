24dd69ad6c6c2929b087c74d62f57f83eec7316c
morgan88888 <55180693+morgan88888@users.noreply.github.com>
2019-10-30 16:00:50 +0800

    動畫

---
+++ G8Unity_project/Assets/chicken.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chicken : MonoBehaviour
+{
+    int speed = 10; // Start is called before the first frame update
+    float turn = 20.5f;
+    bool mission;
+
+}
