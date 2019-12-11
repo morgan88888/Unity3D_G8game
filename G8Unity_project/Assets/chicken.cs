fd8ec0ee17b7878dd27add395e8717ca0703a0f0
morgan88888 <55180693+morgan88888@users.noreply.github.com>
2019-12-11 16:01:34 +0800

    程式的一天

--- G8Unity_project/Assets/chicken.cs
+++ G8Unity_project/Assets/chicken.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 public class chicken : MonoBehaviour
 {
     #region
-    [Header("移動速度")]
-    [Range(1, 100)]
+    [Header("移動速度")][Range(1, 2500)]
     public int speed = 10;
-    [Tooltip("雞主席的旋轉速度"), Range(1.5f, 200f)]
+    [Header("旋轉速度"), Tooltip("雞主席的旋轉速度"), Range(1.5f, 200f)]
     public float turn = 20.5f;
+    [Header("是否完成任務")]
     public bool mission;
-    public string name = "雞主席";
+    [Header("玩家名稱")]
+    public string name = "G8雞";
     #endregion
+     
+    public Transform tran;
+    public Rigidbody rig;
+    public Animator ani;
+
+    private void Update()
+    {
+        Turn();
+        Run();
+    }
 
     private void Run()
-    { 
-    
+    {
+        float v = Input.GetAxis("Vertical");
+        rig.AddForce(tran.forward * speed * v * Time.deltaTime);
     }
     private void Turn()
     {
-
+        float h = Input.GetAxis("Horizontal"); 
+        tran.Rotate(0, turn * h * Time.deltaTime, 0);
     }
     private void Bark()
     {
-
+         
     }
     private void Catch()
     {
